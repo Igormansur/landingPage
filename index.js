dbe9cc2f90384499d3a00ef9d305afdd3eaed889
Igormansur <12502065@aluno.cotemig.com.br>
2026-09-14 07:58:08 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+const botaoTema = document.getElementById("botaoTema");
+
+botaoTema.addEventListener("click", function () {
+
+  document.body.classList.toggle("modo-claro");
+
+  if (document.body.classList.contains("modo-claro")) {
+    botaoTema.textContent = "🌙";
+  } else {
+    botaoTema.textContent = "☀️";
+  }
+
+});
